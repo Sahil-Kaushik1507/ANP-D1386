192ccc42a402d11f1c65814ede18b74b64869a2b
Sahil Kaushik <sahilkaushik1507@gmail.com>
2025-09-15 13:58:54 +0530

    js

---
+++ JS/cal.js
@@ -0,0 +1,86 @@
+var a = 10;
+var b =20;
+
+
+
+// var user =10;
+
+
+// var user = "sahil"
+
+
+// console.log(user);
+
+// const pi =3.14;
+
+// const mystr= "sahil";
+
+console.log(a+b);
+
+b= 30;
+
+
+console.log(a+b);
+
+
+let c = 40;
+let d = 50;
+console.log(c+d);
+
+
+
+
+
+c=80
+console.log(c+d);
+
+
+
+
+
+
+let user = 10;
+user= "hello";
+{
+
+    let user ="sahil";
+    console.log(user)
+
+    var temp=100;
+}
+console.log(user)
+console.log(temp)
+
+
+
+// if(a<b){
+//     console.log("b is greater")
+
+//     if(b>c){
+//             console.log("b is greater")
+//     }
+
+// }else if(){
+//     console.log("a is greater")
+// }else if(){
+
+// }else{
+    
+// }
+
+
+let A= 2;
+
+switch (A){
+
+    case 1: {console.log("TODAY IS mONDAY"); break};
+    case 2 : console.log("TODAY IS mONDAY"); break;
+    case 3 : console.log("TODAY IS mONDAY"); break;
+    case 4 : console.log("TODAY IS mONDAY"); break;
+    case 5 : console.log("TODAY IS mONDAY"); break;
+    case 6 : console.log("TODAY IS mONDAY"); break;
+
+    default: console.log("MAKE PROPER SELECTION")
+}
+
+
---
+++ JS/script.js
@@ -0,0 +1,35 @@
+console.log("Hello World")
+
+
+var a = 10;
+var b = "hello"
+var c = 3.14;
+var d = true;
+
+var e;
+
+var f = null;
+
+var g =null;
+
+// number
+// string
+// BigInt
+// Boolean 
+
+
+//undefined     --> not defined
+
+//null
+//symbol
+
+
+//array [] object {}
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+console.log(typeof g);
